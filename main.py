f5ee03b4ed809b4d9da4c7122ccf43c471544146
Leonardo Coutiño Tamayo <87379933+Leoct97@users.noreply.github.com>
2024-01-06 09:34:28 -0600

    Test GS
    cambios despues de la practica

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-from typing import Union
-from fastapi import FastAPI,HTTPException
+from datetime import date
+from fastapi import FastAPI
 from conexion import datos
 from conexion import connectionString
 from pydantic import BaseModel
 import json
 import pyodbc
 
-app = FastAPI()
-
+app = FastAPI()\
+    
 class articulo(BaseModel):
     id: int
     name: str
     precio: int
-
-
+    
+    
 @app.get("/obtener")
 def read():
     return (datos)
@@ -29,8 +29,6 @@ def insert(articulo: articulo):
     cursor.close()
     return articulo
 
-
-
 @app.get("/inventario")
 def obtener(id: int):
     conn = pyodbc.connect(connectionString)
@@ -45,3 +43,57 @@ def obtener(id: int):
     dato2 = json.dumps(dato2)
     cursor.close()
     return dato2
+
+
+@app.get("/delete")
+def delete(id: int):
+    conn = pyodbc.connect(connectionString)
+    cursor = conn.cursor()
+    query = "delete articulos where id = ?"
+    cursor.execute(query, (id))
+    cursor.commit()
+    cursor.close()
+    return id
+
+
+@app.put("/actualizar")
+def update(id: int, precio: int):
+    conn = pyodbc.connect(connectionString)
+    cursor = conn.cursor()
+    query = "update articulos set precio = ? where id = ?"
+    cursor.execute(query, (precio, id))
+    cursor.commit()
+    cursor.close()
+    return id
+
+
+@app.put("/newinv")
+def newinv(id: int, stock: int):
+    conn = pyodbc.connect(connectionString)
+    cursor = conn.cursor()
+    query = "insert into inventario(id_articulo,fecha,stock) values (?,getdate(),?)"
+    cursor.execute(query, (id, stock))
+    cursor.commit()
+    cursor.close()
+    return id
+
+@app.put("/actualizarinv")
+def updateinv(id: int, precio: int):
+    conn = pyodbc.connect(connectionString)
+    cursor = conn.cursor()
+    query = "update articulos set precio = ? where id = ?"
+    cursor.execute(query, (precio, id))
+    cursor.commit()
+    cursor.close()
+    return id
+
+
+@app.delete("/deleteprod")
+def deleteprod(id: int):
+    conn = pyodbc.connect(connectionString)
+    cursor = conn.cursor()
+    query = "delete from articulos where id = ?"
+    cursor.execute(query, (id))
+    cursor.commit()
+    cursor.close()
+    return id
